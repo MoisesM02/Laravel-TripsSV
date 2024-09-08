5d1330f3afe838fb8d70e96cbfacc0f420de317c
Moisés Marroquín <moisesalonsomarroquinayala@gmail.com>
2024-09-08 00:01:17 -0600

    TinyMCE implemented with CDN and place creation form finished

--- app/Http/Controllers/PlaceController.php
+++ app/Http/Controllers/PlaceController.php
@@ -5,7 +5,10 @@ namespace App\Http\Controllers;
 use App\Models\Place;
 use Illuminate\Http\Request;
 use App\Models\Category;
-
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Validation\Rule;
+use Illuminate\Validation\Rules\File;
 class PlaceController extends Controller
 {
     /**
@@ -30,9 +33,43 @@ class PlaceController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(Request $request):RedirectResponse
     {
         //
+        $path;
+        $validated = Validator::validate($request->all(), [
+            'name' => 'required|string|max:255',
+            'description' => 'required|string|max:255',
+            'content' => 'required',
+            'category_id' => 'required',
+            'image' => [
+                'required',
+                File::image()
+                ->min('1kb')
+                ->max('5mb'),
+                Rule::dimensions()->maxWidth(1350)->maxHeight(1080),
+            ],
+            
+            ]);
+        
+        if($request->hasFile('image')){
+            $image = $request->file('image');
+            $path = $request->file('image')->store('images', 'public'); 
+            //Guarda la imagen dentro del directorio storage/images/
+            //Para mostrar la imagen en blade se debe de utilizar la función asset() con el parámetro 'storage/' . $category->image_path
+            
+        }else{
+            return redirect(route('places.create'));
+        }
+        Place::create([
+            "name" => $request->name,
+            "description" => $request->description,
+            "image_path" => $path,
+            "content" => $request->input('content'),
+            "category_id" => $request->category_id,
+            "user_id" => $request->user()->id,
+        ]);
+        return redirect(route('places.create'))->with('success', 'Place created successfully!');
     }
 
     /**
--- resources/views/places/create.blade.php
+++ resources/views/places/create.blade.php
@@ -1,12 +1,22 @@
 <x-app-layout>
     <x-slot name="slot">
 
-    <x-form enctype="multipart/form-data" :route="route('places.create')">
+    <x-form enctype="multipart/form-data" method="POST" :route="route('places.store')">
         @csrf
         @if (session('success'))
         <div class="p-4 mb-4 text-sm text-green-800 rounded-lg bg-green-50 dark:bg-gray-800 dark:text-green-400" role="alert">
                 <span class="font-medium">Success!</span> {{session('success')}}
             </div>
+        @endif
+        @if ($errors->any())
+        <div class="p-4 mt-5 mb-4 text-sm text-red-800 rounded-lg bg-red-50 dark:bg-gray-800 dark:text-red-400" role="alert">
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+            
         @endif
         
         <div class="mb-5">
@@ -39,24 +49,43 @@
         </div>
 
         <div class="mb-4">
-            <x-input-label value ="content" for="content"/>
+            <x-input-label value="Content" for="content"/>
             <textarea name="content" id="content">{{old('content')}}</textarea>
         </div>
         <x-primary-button>
             {{__('Create')}}
         </x-primary-button>
-        @if ($errors->any())
-        <div class="p-4 mt-5 mb-4 text-sm text-red-800 rounded-lg bg-red-50 dark:bg-gray-800 dark:text-red-400" role="alert">
-            <ul>
-                @foreach ($errors->all() as $error)
-                    <li>{{ $error }}</li>
-                @endforeach
-            </ul>
-        </div>
-            <div class="alert alert-danger">
-                
-            </div>
-        @endif
+        
     </x-form>
+
+    <!-- Include TinyMCE from the public directory -->
+
+<!-- Place the first <script> tag in your HTML's <head> -->
+<script src="https://cdn.tiny.cloud/1/{{env('TINYMCE_API_KEY')}}/tinymce/7/tinymce.min.js" referrerpolicy="origin"></script>
+
+<!-- Place the following <script> and <textarea> tags your HTML's <body> -->
+<script>
+  tinymce.init({
+    selector: 'textarea#content',
+    plugins: [
+      // Core editing features
+      'anchor', 'autolink', 'charmap', 'codesample', 'emoticons', 'image', 'link', 'lists', 'media', 'searchreplace', 'table', 'visualblocks', 'wordcount',
+      // Your account includes a free trial of TinyMCE premium features
+      // Try the most popular premium features until Sep 21, 2024:
+      'checklist', 'mediaembed', 'casechange', 'export', 'formatpainter', 'pageembed', 'a11ychecker', 'tinymcespellchecker', 'permanentpen', 'powerpaste', 'advtable', 'advcode', 'editimage', 'advtemplate', 'ai', 'mentions', 'tinycomments', 'tableofcontents', 'footnotes', 'mergetags', 'autocorrect', 'typography', 'inlinecss', 'markdown',
+    ],
+    toolbar: 'undo redo | blocks fontfamily fontsize | bold italic underline strikethrough | link image media table mergetags | addcomment showcomments | spellcheckdialog a11ycheck typography | align lineheight | checklist numlist bullist indent outdent | emoticons charmap | removeformat',
+    tinycomments_mode: 'embedded',
+    tinycomments_author: 'Author name',
+    mergetags_list: [
+      { value: 'First.Name', title: 'First Name' },
+      { value: 'Email', title: 'Email' },
+    ],
+    ai_request: (request, respondWith) => respondWith.string(() => Promise.reject('See docs to implement AI Assistant')),
+  });
+</script>
+
+    
+});
     </x-slot>
 </x-app-layout>
